7dc258284fccf621b4aede042649e4fc02e73d87
alifakbar-mna <alifakbar.mna@gmail.com>
2026-06-24 11:14:49 +0700

    update file

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,13 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'https://monitoring-sa-ciputra.vercel.app', // 👈 Ganti port 3000 ini sesuai port backend local Anda jika ada
+        changeOrigin: true,
+        secure: false,
+      }
+    }
+  }
 })
